2ea0aef6661f2d0df91eda066cfb789664da7f9b
Varga Dániel <varga.daniel@protonmail.com>
2019-04-27 18:18:21 +0200

    Ez már nem kell.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,5 @@ pub mod ui;
 use ui::App;
 
 fn main() {
-    // Helló!
-    // Ez a Rozsda IDE-vel lett írva!
-    println!("Hello, world!");
-
     App::new().connect_events().then_execute();
-
-    // És ez is!
-    println!("Kész!");
 }
